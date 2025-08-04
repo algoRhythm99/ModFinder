fe3d9cc411c83821425a75ea2f23e3dfb6ece3bc
xADDBx <62178123+xADDBx@users.noreply.github.com>
2025-08-05 00:49:37 +0200

    Impl

--- ModFinderClient/Util/Settings.cs
+++ ModFinderClient/Util/Settings.cs
@@ -4,6 +4,7 @@
   {
     public string AutoRTPath { get; set; }
     public string RTPath { get; set; }
+    public string Slug { get; set; }
 
     private static Settings _Instance;
     public static Settings Load()
@@ -13,7 +14,10 @@
         if (Main.TryReadFile("Settings.json", out var settingsRaw))
           _Instance = IOTool.FromString<Settings>(settingsRaw);
         else
+        {
           _Instance = new();
+          _Instance.Save();
+        }
       }
 
       return _Instance;
