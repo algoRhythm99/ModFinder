a6aaa8c387dbdc26da2928ecea28ff2b3df2c384
xADDBx <62178123+xADDBx@users.noreply.github.com>
2025-08-26 16:35:38 +0200

    Implement SSO

--- ModFinderClient/Util/Settings.cs
+++ ModFinderClient/Util/Settings.cs
@@ -1,10 +1,22 @@
-﻿namespace ModFinder.Util
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace ModFinder.Util
 {
   public class Settings
   {
     public string AutoRTPath { get; set; }
     public string RTPath { get; set; }
-    public string NexusApiKey { get; set; }
+    public byte[] NexusApiKeyBytes { get; set; }
+    public string MaybeGetNexusKey()
+    {
+      if (NexusApiKeyBytes == null)
+      {
+        return null;
+      }
+      var plain = ProtectedData.Unprotect(NexusApiKeyBytes, null, DataProtectionScope.CurrentUser);
+      return Encoding.UTF8.GetString(plain);
+    }
 
     private static Settings _Instance;
     public static Settings Load()
