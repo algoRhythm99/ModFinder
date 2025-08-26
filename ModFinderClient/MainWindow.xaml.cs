fe94f7861e7a72eda7636f188ff93a5c16063137
xADDBx <62178123+xADDBx@users.noreply.github.com>
2025-08-26 17:50:29 +0200

    Handle non-premium

--- ModFinderClient/MainWindow.xaml.cs
+++ ModFinderClient/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using ModFinder.Mod;
 using ModFinder.UI;
 using ModFinder.Util;
+using NexusModsNET;
 using System;
 using System.Buffers;
 using System.Collections;
@@ -870,10 +871,20 @@ namespace ModFinder
           */
           try
           {
-            var bytes = Encoding.UTF8.GetBytes(sb.ToString());
-            Main.Settings.NexusApiKeyBytes = ProtectedData.Protect(bytes, null, DataProtectionScope.CurrentUser);
-            Main.Settings.Save();
-            break;
+            var key = sb.ToString();
+            var client = NexusModsFactory.New(key, "Modfinder", Main.ProductVersion);
+            var user = await client.CreateUserInquirer().GetUserAsync();
+            if (user.IsPremium)
+            {
+              var bytes = Encoding.UTF8.GetBytes(key);
+              Main.Settings.NexusApiKeyBytes = ProtectedData.Protect(bytes, null, DataProtectionScope.CurrentUser);
+              Main.Settings.Save();
+            } 
+            else
+            {
+              Logger.Log.Warning("Tried to link Non-Premium Nexus Account. Only Premium Accounts support direct download!");
+            }
+              break;
           } 
           catch (Exception ex)
           {
